2d2d7b9cd23735cda293a62958c28dcc5586995c
Do Minh Hai <dominhhai1111@gmail.com>
2019-07-22 22:50:41 +0700

    update-grids

--- screens/GridGame.js
+++ screens/GridGame.js
@@ -9,34 +9,106 @@ import {
 	Container,
 	Row,
 	Col,
-	Text
+	Text,
+	TouchableWithoutFeedback,
+	TouchableOpacity,
+	TouchableHighlight,
 } from 'react-native';
+
+const STATUS_START = 1;
+const STATUS_SHOWING = 2;
+const STATUS_ANSWERING =3;
+const STATUS_CONTINUE = 4;
+const STATUS_FINISH = 5;
+
+const BG_COLOR_NORMAL = 1;
+const BG_COLOR_TOUCHING = 2;
+const BG_COLOR_INCORRECT = 3;
 
 class Grid extends Component {
 	state = {
-		bgColor: true,
-		duration: 1000,
+		bgColor: BG_COLOR_NORMAL,
+		duration: 20,
+		enableTouchGrid: false,
+		bgColors: {
+			[BG_COLOR_NORMAL]: 'bg_normal',
+			[BG_COLOR_TOUCHING]: 'bg_touching',
+			[BG_COLOR_INCORRECT]: 'bg_incorrect',
+		},
+		status: STATUS_SHOWING,
 	};
 
 	componentDidMount() {
-		let timerId = setInterval(()=>(
-				this.setState(previousState => (
-					{
-						bgColor: !previousState.bgColor
-					}
-				))
-			), this.state.duration);
+		let timerId = setInterval(()=>{
+			if (this.state.status == STATUS_SHOWING) {
+				this.updateColor();
+			}
+		}, this.state.duration);
+	}
 
-		setTimeout(() => {
-			clearInterval(timerId), this.state.duration
+	updateColor() {
+		if (this.props.changeColor) {
+			this.setState(previousState => (
+				{
+					bgColor: BG_COLOR_TOUCHING,
+				}
+			));
+		} else {
+			this.setState(previousState => (
+				{
+					bgColor: BG_COLOR_NORMAL,
+				}
+			));
+		}
+	}
+
+	enableTouchGrid(enableTouchGrid) {
+		this.setState(
+				{
+					'enableTouchGrid': enableTouchGrid
+				}
+			);
+	}
+
+	setColor(color) {
+		if (!((this.state.status == STATUS_ANSWERING || this.state.status == STATUS_FINISH) 
+			&& this.state.bgColor == BG_COLOR_INCORRECT)) {
+			this.setState(previousState => (
+				{
+					bgColor: color,
+				}
+			));
+		} 
+	}
+
+	setStatus(status) {
+		this.setState({
+			'status': status
 		});
+
+		if (status == STATUS_ANSWERING) {
+			this.setColor(BG_COLOR_NORMAL);
+			this.enableTouchGrid(true);
+		}
+
+		if (status == STATUS_FINISH) {
+			this.enableTouchGrid(false);
+		}
 	}
 
 	render() {
-		const gridStyle = this.state.bgColor ? styles.bg_info : styles.bg_warning;
+		const gridStyle = styles[this.state.bgColors[this.state.bgColor]];
 
 		return (
-			<View style={[gridStyle, styles.grid]}></View>
+			<TouchableHighlight 
+				onPress={ () => this.props.onUpdate(this.props.gridId) } 
+				disabled={ !this.state.enableTouchGrid }
+				onShowUnderlay={ () => this.setColor(BG_COLOR_TOUCHING) }
+				onHideUnderlay={ () => this.setColor(BG_COLOR_NORMAL) } 
+			>
+				<View style={[gridStyle, styles.grid]}></View>
+			</TouchableHighlight>
+			
 		);
 	}
 }
@@ -49,22 +121,129 @@ export default class GridGame extends Component<Props> {
 	constructor(props) {
 	  super(props);
 	
-	  this.state = {};
+	  this.state = {
+	  	stepDuration: 100,
+	  	numbers: 9,
+	  	stepsNumber: 16,
+	  	currentNumber: 1,
+	  	currentCheckedNumber: 1,
+	  	steps: {
+	  		1: 1, 
+	  		2: 2, 
+	  		3: 3, 
+	  		4: 6, 
+	  		5: 9, 
+	  		6: 8, 
+	  		7: 7, 
+	  		8: 4, 
+	  		9: 5,
+	  		10: 4, 
+	  		11: 7, 
+	  		12: 8, 
+	  		13: 9, 
+	  		14: 6, 
+	  		15: 3, 
+	  		16: 2,
+	  	},
+	  	changeColorGrids: {
+	  		1: false,
+	  		2: false,
+	  		3: false,
+	  		4: false,
+	  		5: false,
+	  		6: false,
+	  		7: false,
+	  		8: false,
+	  		9: false,
+	  	},
+	  	status: STATUS_SHOWING,
+	  };
+	}
+
+	componentDidMount() {
+		let changeColorGridsTime = setInterval(()=>{
+			if (this.state.status == STATUS_SHOWING) {
+				if (this.state.currentNumber > 1) {
+					this.setState(previousState => ({
+						changeColorGrids: Object.assign({}, previousState.changeColorGrids, {
+					     	[previousState.steps[previousState.currentNumber - 1]]: false,
+					    }),
+					}))
+				}
+				if (this.state.currentNumber > this.state.stepsNumber) {
+					this.setStatus(STATUS_ANSWERING);
+				}
+				this.setState(previousState => ({
+					changeColorGrids: Object.assign({}, previousState.changeColorGrids, {
+				     	[previousState.steps[previousState.currentNumber]]: true,
+				    }),
+					currentNumber: previousState.currentNumber + 1,
+				}))
+			}
+		}, this.state.stepDuration);
+	}
+
+	setStatus(status) {
+		this.setState(previousState => ({
+				status: status,
+			}));
+
+		if (status == STATUS_ANSWERING) {
+			this.setAnsweringStatus();
+		}
+
+		if (status == STATUS_FINISH) {
+			this.setFinishStatus();
+		}
+	}
+
+	createGrids() {
+		let grids = [];
+
+		for (let i = 1; i <= this.state.numbers; i++) {
+			grids.push(
+					<Grid key={ i } 
+						ref={ i } 
+						gridId={ i } 
+						changeColor={ this.state.changeColorGrids[i] } 
+						onUpdate={ this.checkCorrection.bind(this) }
+					/>
+				);
+		}
+
+		return grids;
+	}
+
+	checkCorrection(gridId) {
+		if (gridId != this.state.steps[this.state.currentCheckedNumber]) {
+			this.refs[gridId].setColor(BG_COLOR_INCORRECT);
+			this.setStatus(STATUS_FINISH);
+		} else {
+			this.setState(previousState => ({
+				'currentCheckedNumber': previousState.currentCheckedNumber + 1,
+			}));
+		}
+	}
+
+	setAnsweringStatus() {
+		for (let i = 1; i <= this.state.numbers; i++) {
+			this.refs[i].setStatus(STATUS_ANSWERING);
+		}
+	}
+
+	setFinishStatus() {
+		for (let i = 1; i <= this.state.numbers; i++) {
+			this.refs[i].setStatus(STATUS_FINISH);
+		}
 	}
 
 	render() {
+		let grids = this.createGrids();
+
 		return (
 			<View style={styles.container}>
 				<View style={styles.grid_area}>
-					<Grid />
-					<Grid />
-					<Grid />
-					<Grid />
-					<Grid />
-					<Grid />
-					<Grid />
-					<Grid />
-					<Grid />
+					{ grids }
 				</View>
 			</View>
 		);	
@@ -88,14 +267,14 @@ const styles = {
         height:330,
         margin:'auto',
     },
-    bg_info: {
+    bg_normal: {
 		backgroundColor: '#17a2b8',
     },
-    bg_warning: {
+    bg_touching: {
 		backgroundColor: '#ffc107',
     },
-    bg_primary: {
-		backgroundColor: '#007bff',
+    bg_incorrect: {
+		backgroundColor: 'red',
     },
     grid: {
     	width: 100,
