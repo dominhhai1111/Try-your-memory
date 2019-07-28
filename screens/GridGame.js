9127045f34ff4cbdaf810a37b4d90dfb8122bd73
Do Minh Hai <dominhhai1111@gmail.com>
2019-07-28 23:40:07 +0700

    update-controls

--- screens/GridGame.js
+++ screens/GridGame.js
@@ -18,7 +18,7 @@ import {
 const STATUS_START = 1;
 const STATUS_SHOWING = 2;
 const STATUS_ANSWERING =3;
-const STATUS_CONTINUE = 4;
+const STATUS_WAITING = 4;
 const STATUS_FINISH = 5;
 
 const BG_COLOR_NORMAL = 1;
@@ -89,9 +89,7 @@ class Grid extends Component {
 		if (status == STATUS_ANSWERING) {
 			this.setColor(BG_COLOR_NORMAL);
 			this.enableTouchGrid(true);
-		}
-
-		if (status == STATUS_FINISH) {
+		} else {
 			this.enableTouchGrid(false);
 		}
 	}
@@ -122,11 +120,12 @@ export default class GridGame extends Component<Props> {
 	  super(props);
 	
 	  this.state = {
+	  	level: 1,
 	  	stepDuration: 100,
 	  	numbers: 9,
 	  	stepsNumber: 16,
 	  	currentNumber: 1,
-	  	currentCheckedNumber: 1,
+	  	currentCheckedNumber: 0,
 	  	steps: {
 	  		1: 1, 
 	  		2: 2, 
@@ -157,10 +156,72 @@ export default class GridGame extends Component<Props> {
 	  		9: false,
 	  	},
 	  	status: STATUS_SHOWING,
+		btnControlText: '',
+		changeColorGridsTime: '',
+		test: 0,
+	  };
+
+	  this.btnControlText = {
+	  	[STATUS_START]: 'Start',
+	  	[STATUS_SHOWING]: 'Showing',
+	  	[STATUS_ANSWERING]: 'Answering',
+	  	[STATUS_WAITING]: 'Continue',
+	  	[STATUS_FINISH]: 'Restart',
 	  };
 	}
 
 	componentDidMount() {
+		this.setStatus(STATUS_START);
+	}
+
+	onPressBtnControl() {
+		if (this.state.status == STATUS_START) {
+			this.setStatus(STATUS_SHOWING);
+		}
+	}
+
+	setBtnControl(status) {
+		this.setState({
+			btnControlText: this.btnControlText[status],
+		});
+	}
+
+	setStatus(status) {
+		this.setState(previousState => ({
+				status: status,
+			}));
+
+		for (let i = 1; i <= this.state.numbers; i++) {
+			this.refs[i].setStatus(status);
+		}
+
+		this.setBtnControl(status);
+
+		if (status == STATUS_START) {
+			this.setStartStatus();
+		}
+
+		if (status == STATUS_SHOWING) {
+			this.setShowingStatus();
+		}
+
+		if (status == STATUS_ANSWERING) {
+			this.setAnsweringStatus();
+		}
+
+		if (status == STATUS_WAITING) {
+			this.setWaitingStatus();
+		}
+
+		if (status == STATUS_FINISH) {
+			this.setFinishStatus();
+		}
+	}
+
+	setStartStatus() {
+	}
+
+	async setShowingStatus() {
 		let changeColorGridsTime = setInterval(()=>{
 			if (this.state.status == STATUS_SHOWING) {
 				if (this.state.currentNumber > 1) {
@@ -183,18 +244,14 @@ export default class GridGame extends Component<Props> {
 		}, this.state.stepDuration);
 	}
 
-	setStatus(status) {
-		this.setState(previousState => ({
-				status: status,
-			}));
+	setAnsweringStatus() {
+		clearInterval(this.state.changeColorGridsTime);
+	}
 
-		if (status == STATUS_ANSWERING) {
-			this.setAnsweringStatus();
-		}
+	setWaitingStatus() {
+	}
 
-		if (status == STATUS_FINISH) {
-			this.setFinishStatus();
-		}
+	setFinishStatus() {
 	}
 
 	createGrids() {
@@ -214,26 +271,18 @@ export default class GridGame extends Component<Props> {
 		return grids;
 	}
 
-	checkCorrection(gridId) {
+	async checkCorrection(gridId) {
+		await this.setState(previousState => ({
+				'currentCheckedNumber': previousState.currentCheckedNumber + 1,
+			}));
+
 		if (gridId != this.state.steps[this.state.currentCheckedNumber]) {
 			this.refs[gridId].setColor(BG_COLOR_INCORRECT);
 			this.setStatus(STATUS_FINISH);
 		} else {
-			this.setState(previousState => ({
-				'currentCheckedNumber': previousState.currentCheckedNumber + 1,
-			}));
-		}
-	}
-
-	setAnsweringStatus() {
-		for (let i = 1; i <= this.state.numbers; i++) {
-			this.refs[i].setStatus(STATUS_ANSWERING);
-		}
-	}
-
-	setFinishStatus() {
-		for (let i = 1; i <= this.state.numbers; i++) {
-			this.refs[i].setStatus(STATUS_FINISH);
+			if (this.state.currentCheckedNumber == this.state.stepsNumber) {
+				this.setStatus(STATUS_WAITING);
+			}
 		}
 	}
 
@@ -242,21 +291,50 @@ export default class GridGame extends Component<Props> {
 
 		return (
 			<View style={styles.container}>
-				<View style={styles.grid_area}>
-					{ grids }
+				<View style = { styles.score_area_bound }>
+					<View style = { styles.score_area }>
+						<Text style = { styles.level }>Level: { this. state.level }</Text>
+						<Text style = { styles.step }>Step: { this.state.currentCheckedNumber }/{ this.state.stepsNumber }</Text>
+					</View>
+				</View>
+				
+				<View style = { styles.grid_area_bound }>
+					<View style={styles.grid_area}>
+						{ grids }
+					</View>
+				</View>
+				
+				<View style = { styles.btn_area_bound}>
+					<TouchableOpacity
+						style = { styles.btn_control }
+						onPress = { this.onPressBtnControl.bind(this) }
+					>
+						<Text style = { styles.btn_control_text }>{ this.state.btnControlText }</Text>
+					</TouchableOpacity>
 				</View>
 			</View>
-		);	
+		);
 	}
 }
 
 const styles = {
 	container: {
 		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
+		padding: 15,
 	},
- 	grid_area: {
+    score_area_bound: {
+    	marginBottom: 30,
+    	alignItems: 'center',
+    },
+    score_area: {
+    	flexDirection: 'row',
+    	justifyContent: 'space-between',
+    	width: 300,
+    },
+    grid_area_bound: {
+    	justifyContent: 'center',
+    },
+    grid_area: {
  		justifyContent: 'center',
         alignItems: 'center',
         alignContent: 'center',
@@ -266,6 +344,14 @@ const styles = {
   		width: 330,
         height:330,
         margin:'auto',
+    },
+    level: {
+    	fontSize: 20,
+    	fontWeight: 'bold',
+    },
+    step: {
+    	fontSize: 20,
+    	fontWeight: 'bold',
     },
     bg_normal: {
 		backgroundColor: '#17a2b8',
@@ -283,5 +369,24 @@ const styles = {
         borderWidth: 1,
         borderStyle: 'solid',
         position: 'relative',
-    }
+    },
+    btn_area_bound: {
+    	marginTop: 30,
+    	alignItems: 'center',
+    },
+    btn_control: {
+    	paddingTop: 10,
+    	paddingBottom: 10,
+    	paddingLeft: 20,
+    	paddingRight: 20,
+    	backgroundColor: 'yellow',
+    	borderColor: 'red',
+    	borderWidth: 3,
+    	borderStyle: 'solid',
+    	borderRadius: 10,
+    },
+    btn_control_text: {
+		fontSize: 20,
+    	fontWeight: 'bold', 
+    },
 };
